c0d70c98a5c04b445e0b32990809b96fbf3102b2
jittrawanpadsang <s6812022611201@email.kmutnb.ac.th>
2025-08-29 16:24:16 +0700

    Add files via upload

---
+++ codingw7_03.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+    int level;
+    printf("Enter level (1-4): ");
+    scanf("%d", &level);
+
+
+    if (level <= 0);{
+        printf("The level below 1 is not allowed\n");
+    } 
+     if (level > 4); {
+        printf("The level above 4 is not allowed\n");
+    } 
+    {
+
+    switch (level) {
+        case 1: printf("Beginner\n"); break;
+        case 2: printf("Intermediate\n"); break;
+        case 3: printf("Advanced\n"); break;
+        case 4: printf("Expert\n"); break;
+        }
+    }
+
+    return 0;
+
+}
